b4fe8f04e2b676f9b418605c58bd6560da47da39
Daniel Montes <daniel.montes@gmail.com>
2022-08-19 21:42:17 +0200

    Add info result to bad requests tests

--- src/server/middlewares/errors/errors.js
+++ src/server/middlewares/errors/errors.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const chalk = require("chalk");
 const debug = require("debug")("AAP:Errors");
-const { ValidationError } = require("express-validation");
+// const { ValidationError } = require("express-validation");
 
 const { customError } = require("../../utils/customError");
 
@@ -21,17 +21,19 @@ const generalError = (err, req, res, next) => {
       .status(404)
       .json("Please visit site: https://adoptapenguin.netlify.app/");
     debug(chalk.red(`User Request--> ERROR: ${err.customMessage}`));
-  } else if (err instanceof ValidationError) {
-    debug(
-      `User Request--> ${chalk.red(
-        `ERROR: (${err.statusCode}) ${err.message} - ${req.body}`
-      )}`
-    );
-    res.status(400).json({ message: "Validation error" });
   } else {
-    debug(chalk.red(`User Request--> ERROR: ${err}`));
-
-    res.status(errorCode).json({ message: errorMessage });
+    if (err.details) {
+      debug(
+        `User Request--> ${chalk.red(
+          `ERROR: (${err.statusCode}) ${err.message}`
+        )}`
+      );
+    }
+    res
+      .status(err.status)
+      .json(
+        `message: ${errorMessage}, code: ${errorCode} },status: ${err.status}`
+      );
   }
 };
 
