794fe70a0b44a763e8a7217ce291fe5c5edadb4e
Daniel Montes <daniel.montes@gmail.com>
2022-11-28 13:45:29 +0100

    Resize upload foto

--- src/server/schemas/penguinSchema.js
+++ src/server/schemas/penguinSchema.js
@@ -5,6 +5,8 @@ const penguinSchema = {
     name: Joi.string().max(20),
     category: Joi.string().max(20),
     image: Joi.string().allow(null, ""),
+    imageBackup: Joi.string().allow(null, ""),
+    imageResized: Joi.string().allow(null, ""),
     likes: Joi.number().integer(),
     description: Joi.string().max(200),
     owner: Joi.array().items(Joi.string().max(20)),
